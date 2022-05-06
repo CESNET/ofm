b78717b6fe836b07c213d4c68aac84a80437afdf
Radek Isa <isa@cesnet.cz>
2022-05-06 14:26:47 +0200

    MFB SPLITER [BUGFIX] small fix of signal assign. Cut verification time by reduce number of input transactions

--- comp/mfb_tools/flow/splitter_simple/uvm/ver_settings.py
+++ comp/mfb_tools/flow/splitter_simple/uvm/ver_settings.py
@@ -9,7 +9,7 @@ SETTINGS = {
         "REGION_SIZE"        : "8",
         "BLOCK_SIZE"         : "8",
         "ITEM_WIDTH"         : "8",
-        "TRANSACTION_COUNT"  : "10000",
+        "TRANSACTION_COUNT"  : "3000",
         "META_BEHAV"         : "1",
     },
     "pcie" : {
@@ -27,12 +27,6 @@ SETTINGS = {
     "region_comb_2" : {
         "REGIONS"            : "2",
         "REGION_SIZE"        : "8",
-        "BLOCK_SIZE"         : "8",
-        "ITEM_WIDTH"         : "8",
-    },
-    "region_comb_3" : {
-        "REGIONS"            : "1",
-        "REGION_SIZE"        : "1",
         "BLOCK_SIZE"         : "8",
         "ITEM_WIDTH"         : "8",
     },
@@ -72,9 +66,8 @@ SETTINGS = {
     (), # Works the same as '("default",),' as the "default" is applied in every combination
     ("region_comb_1", "outputs_2", "meta_sop",),
     ("region_comb_2", "outputs_5", "meta_eop",),
-    ("region_comb_3", "meta_sop",),
     ("region_comb_4", "outputs_2", "meta_eop",),
-    ("region_comb_5", "outputs_5", "meta_sop",),
+    ("region_comb_5", "meta_sop",),
     ("region_comb_6", "meta_eop",),
     ("pcie", "meta_sop",),
     ),
