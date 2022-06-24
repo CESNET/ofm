24673cfefac5994894e1cfb3bedcdffc7a13dc67
Radek Isa <isa@cesnet.cz>
2022-06-24 10:01:17 +0200

    UVM: [BUGFIX] Load UVM MVB framework instead UVM MFB framework

--- comp/uvm/logic_vector_mvb/Modules.tcl
+++ comp/uvm/logic_vector_mvb/Modules.tcl
@@ -5,6 +5,6 @@
 
 lappend COMPONENTS [list "RESET"           "$OFM_PATH/comp/uvm/reset"          "FULL"]
 lappend COMPONENTS [list "LOGIC_VECTOR"    "$OFM_PATH/comp/uvm/logic_vector"   "FULL"]
-lappend COMPONENTS [list "MVB"             "$OFM_PATH/comp/uvm/mfb"            "FULL"]
+lappend COMPONENTS [list "MVB"             "$OFM_PATH/comp/uvm/mvb"            "FULL"]
 
 lappend MOD "$ENTITY_BASE/pkg.sv"
