d9edc0aa9a73ad6a40c9d9fdf228ed0b917babc3
Vladislav Valek <xvalek14@vutbr.cz>
2022-07-27 15:59:01 +0200

    ptc_mfb2pcie_axi [FEATURE]: the design adjusted to be more general
    The design was remade to configurate the output according to the number
    of regions given by the generic parameter and not by the DEVICE generic.
    That is because the UltraScale devices can also have the disabled
    Straddling option on the 256b variant of the bus. The AXI_DATA_WIDTH
    generic parameter has also been removed because some values of this
    generic can clash with MFB parameters being set so the latter has been
    kept because it allows for more fine grained configuration.
    
    The CLK and RESET ports have been removed because the component is not
    synchronous and the RESET was not used at all. Those ports and above
    mentioned generics have also been removed from this component
    instantiation in the PTC module.

--- comp/pcie/ptc/comp/mfb2pcie_axi/ptc_mfb2pcie_axi.vhd
+++ comp/pcie/ptc/comp/mfb2pcie_axi/ptc_mfb2pcie_axi.vhd
@@ -1,6 +1,7 @@
 -- ptc_mfb2pcie_axi.vhd: MFB to PCIE AXI convertor
 -- Copyright (C) 2018 CESNET
 -- Author(s): Jakub Cabal <cabal@cesnet.cz>
+--            Vladislav Valek <valekv@cesnet.cz>
 --
 -- SPDX-License-Identifier: BSD-3-Clause
 --
@@ -15,15 +16,9 @@ use work.type_pack.all;
 entity PTC_MFB2PCIE_AXI is
    generic(
       -- =======================================================================
-      -- Target device specification
-      -- =======================================================================
-      -- Supported devices: "7SERIES", "ULTRASCALE"
-      DEVICE           : string  := "ULTRASCALE";
-      -- =======================================================================
       -- MFB BUS CONFIGURATION: 
       -- =======================================================================
-      -- Supported configuration is MFB(2,1,8,32) for PCIe on UltraScale+
-      -- Supported configuration is MFB(1,1,8,32) for PCIe on Virtex 7 Series
+      -- Supported configurations are: (2,1,8,32), (1,1,8,32)
       MFB_REGIONS      : natural := 2;
       MFB_REGION_SIZE  : natural := 1;
       MFB_BLOCK_SIZE   : natural := 8;
@@ -31,63 +26,45 @@ entity PTC_MFB2PCIE_AXI is
       -- =======================================================================
       -- AXI BUS CONFIGURATION: 
       -- =======================================================================
-      -- DATA=512, RQ=137 for Gen3x16 PCIe (Virtex UltraScale+) - with straddling!
-      -- DATA=256, RQ=60  for Gen3x16 PCIe (Virtex 7 Series) - with straddling!
-      AXI_DATA_WIDTH   : natural := 512;
+      -- RQ_USER_WIDTH = 137 for Gen3x16 PCIe - with straddling!
+      -- RQ_USER_WIDTH = 62  for Gen3x8 PCIe - without straddling!
+      -- RQ_USER_WIDTH = 60  for Gen3x8 PCIe - without straddling!
       AXI_RQUSER_WIDTH : natural := 137
-
-      -- UltraScale+ RQ user:
-      -- (
-      -- TODO
-      -- -1 downto 0)
-
-      -- Virtex 7 Series RQ user:
-      -- (
-      -- MFB_REGOINS*MFB_REG_SIZE*MFB_BLOCK_SIZE*MFB_ITEM_WIDTH/8      -- parity (zeros)
-      -- 4                                                             -- seq num (zeros)
-      -- 12                                                            -- tph (zeros)
-      -- 1                                                             -- discontinue (error) (zero)
-      -- 3                                                             -- Addr Offset (zeros)
-      -- (MFB_REGIONS*MFB_REG_SIZE*MFB_BLOCK_SIZE*MFB_ITEM_WIDTH)/8    -- Last byte Enable
-      -- (MFB_REGIONS*MFB_REG_SIZE*MFB_BLOCK_SIZE*MFB_ITEM_WIDTH)/8    -- First byte Enable
-      -- -1 downto 0)
-   );
+      );
    port(
-      -- =======================================================================
-      -- CLOCK AND RESET
-      -- =======================================================================
-      CLK            : in  std_logic;
-      RESET          : in  std_logic;
       -- =======================================================================
       -- INPUT MFB INTERFACE
       -- =======================================================================
-      RX_MFB_DATA    : in  std_logic_vector(MFB_REGIONS*MFB_REGION_SIZE*MFB_BLOCK_SIZE*MFB_ITEM_WIDTH-1 downto 0);
-      RX_MFB_SOF_POS : in  std_logic_vector(MFB_REGIONS*max(1,log2(MFB_REGION_SIZE))-1 downto 0);
-      RX_MFB_EOF_POS : in  std_logic_vector(MFB_REGIONS*max(1,log2(MFB_REGION_SIZE*MFB_BLOCK_SIZE))-1 downto 0);
-      RX_MFB_SOF     : in  std_logic_vector(MFB_REGIONS-1 downto 0);
-      RX_MFB_EOF     : in  std_logic_vector(MFB_REGIONS-1 downto 0);
+      RX_MFB_DATA    : in  std_logic_vector(MFB_REGIONS*MFB_REGION_SIZE*MFB_BLOCK_SIZE*MFB_ITEM_WIDTH -1 downto 0);
+      RX_MFB_SOF_POS : in  std_logic_vector(MFB_REGIONS*max(1, log2(MFB_REGION_SIZE)) -1 downto 0);
+      RX_MFB_EOF_POS : in  std_logic_vector(MFB_REGIONS*max(1, log2(MFB_REGION_SIZE*MFB_BLOCK_SIZE)) -1 downto 0);
+      RX_MFB_SOF     : in  std_logic_vector(MFB_REGIONS -1 downto 0);
+      RX_MFB_EOF     : in  std_logic_vector(MFB_REGIONS -1 downto 0);
       RX_MFB_SRC_RDY : in  std_logic;
       RX_MFB_DST_RDY : out std_logic;
       -- PCIe transaction length last and first DWORD byte enable (MSB -> last byte, LSB -> first byte)
-      RX_MFB_BE      : in  std_logic_vector(MFB_REGIONS*8-1 downto 0);
+      RX_MFB_BE      : in  std_logic_vector(MFB_REGIONS*8 -1 downto 0);
+      --============================================================================================
+
       -- =======================================================================
       -- OUTPUT AXI REQUESTER REQUEST INTERFACE (RQ)
       -- =======================================================================
       -- Data bus
-      RQ_DATA        : out std_logic_vector(AXI_DATA_WIDTH-1 downto 0);
+      RQ_DATA  : out std_logic_vector(MFB_REGIONS*MFB_REGION_SIZE*MFB_BLOCK_SIZE*MFB_ITEM_WIDTH -1 downto 0);
       -- Set of signals with sideband information about transferred transaction
-      RQ_USER        : out std_logic_vector(AXI_RQUSER_WIDTH-1 downto 0);
+      RQ_USER  : out std_logic_vector(AXI_RQUSER_WIDTH -1 downto 0);
       -- Indication of the last word of a transaction
-      RQ_LAST        : out std_logic;
+      RQ_LAST  : out std_logic;
       -- Indication of valid data
       -- each bit determines validity of different Dword (1 Dword = 4 Bytes)
-      RQ_KEEP        : out std_logic_vector(AXI_DATA_WIDTH/32-1 downto 0);
+      RQ_KEEP  : out std_logic_vector((MFB_REGIONS*MFB_REGION_SIZE*MFB_BLOCK_SIZE*MFB_ITEM_WIDTH)/32 -1 downto 0);
       -- PCIe core is ready to receive a transaction
-      RQ_READY       : in  std_logic;
+      RQ_READY : in  std_logic;
       -- User application sends valid data
-      RQ_VALID       : out std_logic
-   );
-end PTC_MFB2PCIE_AXI;
+      RQ_VALID : out std_logic
+    --============================================================================================
+      );
+end entity;
 
 architecture FULL of PTC_MFB2PCIE_AXI is
 
@@ -96,29 +73,37 @@ architecture FULL of PTC_MFB2PCIE_AXI is
    signal s_rx_mfb_eof_pos_arr : slv_array_t(MFB_REGIONS-1 downto 0)(EOF_POS_WIDTH-1 downto 0);
    signal s_rx_mfb_be_arr      : slv_array_t(MFB_REGIONS-1 downto 0)(8-1 downto 0);
 
-   signal s_rq_last            : std_logic;
-   signal s_rq_keep            : std_logic_vector(AXI_DATA_WIDTH/32-1 downto 0);
+   signal s_rq_last : std_logic;
+   signal s_rq_keep : std_logic_vector(RQ_DATA'length/32-1 downto 0);
 
-   signal s_is_sop             : std_logic_vector(1 downto 0);
-   signal s_is_sop0_ptr        : std_logic_vector(1 downto 0);
-   signal s_is_sop1_ptr        : std_logic_vector(1 downto 0);
+   signal s_is_sop      : std_logic_vector(1 downto 0);
+   signal s_is_sop0_ptr : std_logic_vector(1 downto 0);
+   signal s_is_sop1_ptr : std_logic_vector(1 downto 0);
 
-   signal s_is_eop             : std_logic_vector(1 downto 0);
-   signal s_is_eop0_ptr        : std_logic_vector(3 downto 0);
-   signal s_is_eop1_ptr        : std_logic_vector(3 downto 0);
+   signal s_is_eop      : std_logic_vector(1 downto 0);
+   signal s_is_eop0_ptr : std_logic_vector(3 downto 0);
+   signal s_is_eop1_ptr : std_logic_vector(3 downto 0);
 
-   signal s_first_be0          : std_logic_vector(3 downto 0);
-   signal s_last_be0           : std_logic_vector(3 downto 0);
+   signal s_first_be0 : std_logic_vector(3 downto 0);
+   signal s_last_be0  : std_logic_vector(3 downto 0);
 
-   signal s_first_be1          : std_logic_vector(3 downto 0);
-   signal s_last_be1           : std_logic_vector(3 downto 0);
+   signal s_first_be1 : std_logic_vector(3 downto 0);
+   signal s_last_be1  : std_logic_vector(3 downto 0);
 
-   signal s_first_be           : std_logic_vector(7 downto 0);
-   signal s_last_be            : std_logic_vector(7 downto 0);
+   signal s_first_be : std_logic_vector(7 downto 0);
+   signal s_last_be  : std_logic_vector(7 downto 0);
 
-   signal s_rq_user            : std_logic_vector(AXI_RQUSER_WIDTH-1 downto 0);
+   signal s_rq_user : std_logic_vector(AXI_RQUSER_WIDTH-1 downto 0);
 
 begin
+
+   assert ((MFB_REGIONS = 1 or MFB_REGIONS = 2) and MFB_REGION_SIZE = 1 and MFB_BLOCK_SIZE = 8 and MFB_ITEM_WIDTH = 32)
+      report "PTC_MFB2PCIE_AXI: Unsupported MFB configuration, the allowed are: (2,1,8,32), (1,1,8,32)"
+      severity FAILURE;
+
+   assert (AXI_RQUSER_WIDTH = 60 or AXI_RQUSER_WIDTH = 62 or AXI_RQUSER_WIDTH = 137)
+      report "PTC_MFB2PCIE_AXI: Unsupported AXI RQ USER port width, the supported are: 60, 62, 137"
+      severity FAILURE;
 
    -- --------------------------------------------------------------------------
    --  PREPARE AXI SIGNALS
@@ -127,25 +112,25 @@ begin
    -- is high when is word with end of packet, in straddle mode is not useful
    s_rq_last <= or RX_MFB_EOF;
 
-   ultrascale_tuser_gen : if DEVICE="ULTRASCALE" generate
+   two_region_tuser_gen : if (MFB_REGIONS = 2) generate
       -- create array of end of frame position
-      s_rx_mfb_eof_pos_arr <= slv_array_downto_deser(RX_MFB_EOF_POS,MFB_REGIONS,EOF_POS_WIDTH);
+      s_rx_mfb_eof_pos_arr <= slv_array_downto_deser(RX_MFB_EOF_POS, MFB_REGIONS, EOF_POS_WIDTH);
 
       -- create array of byte enables
-      s_rx_mfb_be_arr <= slv_array_downto_deser(RX_MFB_BE,MFB_REGIONS,8);
+      s_rx_mfb_be_arr <= slv_array_downto_deser(RX_MFB_BE, MFB_REGIONS, 8);
 
       -- keep in straddle mode is not useful therefore, all bits are set to VCC
       s_rq_keep <= (others => '1');
 
       -- create SOP flags and pointers for AXI
-      s_is_sop(0) <= or RX_MFB_SOF;
-      s_is_sop(1) <= and RX_MFB_SOF;
+      s_is_sop(0)   <= or RX_MFB_SOF;
+      s_is_sop(1)   <= and RX_MFB_SOF;
       s_is_sop0_ptr <= "10" when (RX_MFB_SOF = "10") else "00";
       s_is_sop1_ptr <= "10";
 
       -- create EOP flags and pointers for AXI
-      s_is_eop(0) <= or RX_MFB_EOF;
-      s_is_eop(1) <= and RX_MFB_EOF;
+      s_is_eop(0)   <= or RX_MFB_EOF;
+      s_is_eop(1)   <= and RX_MFB_EOF;
       s_is_eop0_ptr <= '1' & s_rx_mfb_eof_pos_arr(1) when (RX_MFB_EOF = "10") else '0' & s_rx_mfb_eof_pos_arr(0);
       s_is_eop1_ptr <= '1' & s_rx_mfb_eof_pos_arr(1);
 
@@ -166,20 +151,27 @@ begin
                    s_is_sop1_ptr & s_is_sop0_ptr & s_is_sop & "0000" & s_last_be & s_first_be;
    end generate;
 
-   virtex7series_tuser_gen : if DEVICE="7SERIES" generate
+   one_region_tuser_gen : if (MFB_REGIONS = 1) generate
       -- keep signal serves as valid for each DWORD of RQ_DATA signal
-      s_rq_keep_pr : process (RX_MFB_EOF,RX_MFB_SRC_RDY,RX_MFB_EOF_POS)
+      s_rq_keep_pr : process (all)
       begin
-         if (RX_MFB_SRC_RDY='0') then -- no data
+         if (RX_MFB_SRC_RDY = '0') then  -- no data
+
             s_rq_keep <= (others => '0');
-         elsif (RX_MFB_EOF(0)='1') then -- end of data
+
+         elsif (RX_MFB_EOF(0) = '1') then  -- end of data
+
             s_rq_keep <= (others => '0');
-            for i in 0 to AXI_DATA_WIDTH/32-1 loop
+
+            for i in 0 to RQ_DATA'length/32-1 loop
                s_rq_keep(i) <= '1';
-               exit when (i=to_integer(unsigned(RX_MFB_EOF_POS)));
+               exit when (i = to_integer(unsigned(RX_MFB_EOF_POS)));
             end loop;
-         else -- start or middle of data
+
+         else                           -- start or middle of data
+
             s_rq_keep <= (others => '1');
+
          end if;
       end process;
 
