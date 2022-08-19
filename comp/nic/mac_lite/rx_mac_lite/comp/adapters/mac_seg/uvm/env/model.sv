11b24258d592c2a0837872a6ee2a548c2e02d560
Radek Isa <isa@cesnet.cz>
2022-08-19 11:07:46 +0200

    UVM: [MAITENANCE] add stats keeping of input packet size

--- comp/nic/mac_lite/rx_mac_lite/comp/adapters/mac_seg/uvm/env/model.sv
+++ comp/nic/mac_lite/rx_mac_lite/comp/adapters/mac_seg/uvm/env/model.sv
@@ -20,12 +20,16 @@ class model extends uvm_component;
     uvm_analysis_port#(uvm_byte_array::sequence_item)                 tx_packet;
     uvm_analysis_port#(uvm_logic_vector::sequence_item#(1))           tx_error;
 
+    local uvm_common::stats packet_size_stats;
+
     function new(string name, uvm_component parent = null);
         super.new(name, parent);
         rx_packet = new("rx_packet", this);
         rx_error  = new("rx_error", this);
         tx_packet = new("tx_packet", this);
         tx_error  = new("tx_error", this);
+
+        packet_size_stats = new();
     endfunction
 
     task run();
@@ -43,6 +47,9 @@ class model extends uvm_component;
             rx_packet.get(rx_tr_packet);
             rx_error.get(rx_tr_error);
 
+            //count stats
+            packet_size_stats.next_val(rx_tr_packet.size());
+
             $cast(tx_tr_packet, rx_tr_packet.clone());
 			tx_tr_error = uvm_logic_vector::sequence_item#(1)::type_id::create("model_result_error");
 			{fcs_error, error, status} = rx_tr_error.data;
@@ -56,5 +63,17 @@ class model extends uvm_component;
             end
         end
     endtask
+
+
+    virtual function void report_phase(uvm_phase phase);
+        string msg;
+        real min, max, avg, std_dev;
+
+        packet_size_stats.count(min, max, avg, std_dev);
+
+        $swrite(msg, "\n\tPacket size statistic\n\t\tmin %0d\n\t\tmax %0d\n\t\tavg %0d\n\t\tstd deviation %0d\n", min, max, avg, std_dev);
+        `uvm_info(this.get_full_name(), msg, UVM_NONE);
+    endfunction
+
 endclass
 
