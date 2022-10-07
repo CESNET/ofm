e810e4c97d42120945698c0cb1027cb866194c99
Vladislav Valek <vladislav.valek@email.cz>
2022-10-07 13:29:04 +0200

    dma_calypte/DevTree.tcl [MAINTENANCE]: remove unused dmatype parameter
    The version is now firm for the Calypte core. Previous implementation of
    the procedure used dmatype to determine correct version in the devicetree.

--- comp/dma/dma_calypte/DevTree.tcl
+++ comp/dma/dma_calypte/DevTree.tcl
@@ -1,19 +1,14 @@
 # dts_dma_calypte_ctrl parameters:
 # 1. strtype    - name of the DMA (Medusa or Calypte)
-# 2. dmatype    - index deciding if current DMA is Medusa or Calypte
-# 3. id         - channel ID
-# 4. base       - base address of channel
-# 5. pcie       - index(es) of PCIe endpoint(s) which DMA controller uses.
-proc dts_dma_calypte_ctrl {strtype dmatype dir id base pcie} {
+# 2. id         - channel ID
+# 3. base       - base address of channel
+# 4. pcie       - index(es) of PCIe endpoint(s) which DMA controller uses.
+proc dts_dma_calypte_ctrl {strtype dir id base pcie} {
     set    ret ""
     append ret "dma_ctrl_$strtype" "_$dir$id {"
     append ret "compatible = \"netcope,dma_ctrl_" $strtype "_" $dir "\";"
     append ret "reg = <$base 0x80>;"
-    if {$dmatype == 2 || $dmatype == 3} {
-        append ret "version = <0x00020000>;"
-    } else {
-        append ret "version = <0x00010002>;"
-    }
+    append ret "version = <0x00010000>;"
     append ret "pcie = <$pcie>;"
     append ret "};"
     return $ret
