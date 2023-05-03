0112f4db3343563fede13f15bd52f48790de782f
Radek Isa <isa@cesnet.cz>
2023-05-03 15:29:33 +0200

    VIVADO [HOTFIX] Remove command which leads to segmentation fould

--- build/misc/vivado_wbs_pre.tcl
+++ build/misc/vivado_wbs_pre.tcl
@@ -3,7 +3,7 @@
 foreach i [get_ports -filter { IS_LOC_FIXED == "FALSE" }] {
     puts "Removing unloced port $i"
     disconnect_net -prune -objects $i
-    remove_port $i
+    #remove_port $i
 }
 
 foreach i [get_cells -filter {IS_LOC_FIXED == "FALSE" && REF_NAME == "OBUFT"}] {
