58741913cc666ec2201d26be04d7e2175d71cf7b
Jakub Cabal <cabal@cesnet.cz>
2023-06-21 08:59:44 +0200

    PTC - STORAGE FIFO: [BUGFIX] Use FIFOX Multi as main storage FIFO, due to the efficient removal of empty regions

--- comp/pcie/ptc/comp/storage_fifo/Modules.tcl
+++ comp/pcie/ptc/comp/storage_fifo/Modules.tcl
@@ -7,22 +7,17 @@
 # Set paths
 
 # Paths to components
-set FIFOXM_BASE        "$OFM_PATH/comp/base/fifo/fifox_multi"
-set MFB_TOOLS_BASE     "$OFM_PATH/comp/mfb_tools"
-set AUX_SIG_BASE       "$OFM_PATH/comp/mfb_tools/logic/auxiliary_signals"
+set FIFOXM_BASE  "$OFM_PATH/comp/base/fifo/fifox_multi"
+set AUX_SIG_BASE "$OFM_PATH/comp/mfb_tools/logic/auxiliary_signals"
 
 # Packages
-set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/math_pack.vhd"
-set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/type_pack.vhd"
-set PACKAGES "$PACKAGES $OFM_PATH/comp/base/pkg/dma_bus_pack.vhd"
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/math_pack.vhd"
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/type_pack.vhd"
+lappend PACKAGES "$OFM_PATH/comp/base/pkg/dma_bus_pack.vhd"
 
 # Components
-set COMPONENTS [concat $COMPONENTS [list \
-   [ list "FIFOX_MULTI"       $FIFOXM_BASE                    "FULL" ] \
-   [ list "MVB_FIFOX"        "$OFM_PATH/comp/mvb_tools/storage/fifox"  "FULL" ] \
-   [ list "MFB_FIFOX"        "$MFB_TOOLS_BASE/storage/fifox"  "FULL" ] \
-   [ list "AUXILIARY_SIGNALS" $AUX_SIG_BASE                   "FULL" ] \
-]]
+lappend COMPONENTS [ list "FIFOX_MULTI"       $FIFOXM_BASE  "FULL" ]
+lappend COMPONENTS [ list "AUXILIARY_SIGNALS" $AUX_SIG_BASE "FULL" ]
 
 # Source files for implemented component
-set MOD "$MOD $ENTITY_BASE/ptc_storage_fifo.vhd"
+lappend MOD "$ENTITY_BASE/ptc_storage_fifo.vhd"
