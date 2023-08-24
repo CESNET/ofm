d8afbf7398e3bd1d89cb1e24c00242b70888e530
Daniel Kondys <xkondy00@vutbr.cz>
2023-08-24 16:27:23 +0200

    MFB Packet Delayer [OPT]: use MFB FIFOX and Frame Masker instead of FIFOX Multi

--- comp/mfb_tools/flow/packet_delayer/Modules.tcl
+++ comp/mfb_tools/flow/packet_delayer/Modules.tcl
@@ -5,19 +5,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Paths to components
-set BASE_BASE                "$OFM_PATH/comp/base"
 set MFB_FIFOX_BASE           "$OFM_PATH/comp/mfb_tools/storage/fifox"
-set MVB_LAST_VLD_BASE        "$OFM_PATH/comp/mvb_tools/aggregate/last_vld"
+set MFB_FRAME_MASKER_BASE    "$OFM_PATH/comp/mfb_tools/flow/frame_masker"
 
 # Packages
 lappend PACKAGES "$OFM_PATH/comp/base/pkg/math_pack.vhd"
 lappend PACKAGES "$OFM_PATH/comp/base/pkg/type_pack.vhd"
 
-lappend COMPONENTS [ list "FIFOX_MULTI"       "$BASE_BASE/fifo/fifox_multi"   "FULL" ]
-lappend COMPONENTS [ list "GEN_ENC"           "$BASE_BASE/logic/enc"          "FULL" ]
-lappend COMPONENTS [ list "DSP_COUNTER"       "$BASE_BASE/dsp/dsp_counter"    "FULL" ]
-lappend COMPONENTS [ list "MVB_LAST_VLD"      $MVB_LAST_VLD_BASE              "FULL" ]
-lappend COMPONENTS [ list "MFB_FIFOX"         $MFB_FIFOX_BASE                 "FULL" ]
+# Components
+lappend COMPONENTS [ list "MFB_FIFOX"         $MFB_FIFOX_BASE         "FULL" ]
+lappend COMPONENTS [ list "MFB_FRAME_MASKER"  $MFB_FRAME_MASKER_BASE  "FULL" ]
 
 # Source files for implemented component
 lappend MOD "$ENTITY_BASE/mfb_packet_delayer.vhd"
