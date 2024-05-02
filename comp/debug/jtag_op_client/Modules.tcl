d1f9ccb746b6dafe51379034ae6c8886531c3646
Tomas Hak <hak@cesnet.cz>
2024-05-02 15:11:08 +0200

    JTAG_OP_CLIENT: [MAINTENANCE] cleanup IP file (gen for each card)
    move ip generation to core/cards

--- comp/debug/jtag_op_client/Modules.tcl
+++ comp/debug/jtag_op_client/Modules.tcl
@@ -10,7 +10,6 @@ set MI_ASYNC_BASE "$OFM_PATH/comp/mi_tools/async"
 lappend COMPONENTS [ list "MI2AVMM"  $MI2AVMM_BASE  "FULL" ]
 lappend COMPONENTS [ list "MI_ASYNC" $MI_ASYNC_BASE "FULL" ]
 
-lappend MOD "$ENTITY_BASE/jtag_op_ip.ip"
 lappend MOD "$ENTITY_BASE/jtag_op_client.vhd"
 
 lappend MOD "$ENTITY_BASE/DevTree.tcl"
