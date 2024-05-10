147106cd85734a94258b2b19481425ad06b55911
xschwao00 <xschwao00@stud.fit.vutbr.cz>
2024-05-10 16:15:59 +0200

    MVB drivers and monitors: [FEATURE] add drivers.py, monitors.py and config file utils.py

---
+++ python/cocotbext/cocotbext/ofm/mvb/monitors.py
@@ -0,0 +1,60 @@
+# monitors.py: MVBMonitor
+# Copyright (C) 2024 CESNET z. s. p. o.
+# Author(s): Ondřej Schwarz <OndrejSchwarz@cesnet.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import cocotb
+from cocotb_bus.monitors import BusMonitor
+from cocotb.triggers import RisingEdge
+
+class MVBProtocolError(Exception):
+    pass
+
+class MVBMonitor(BusMonitor):
+    _signals = ["data", "vld", "src_rdy", "dst_rdy"]
+
+    def __init__(self, entity, name, clock, array_idx=None):
+        BusMonitor.__init__(self, entity, name, clock, array_idx=array_idx)
+        self.item_cnt = 0
+        self.clock = clock
+        self._items = len(self.bus.vld)
+        self._word_width = int(len(self.bus.data)/8) #width in bytes
+        self._item_width = int(self._word_width/self._items)
+
+
+    def _is_valid_word(self, signal_src_rdy, signal_dst_rdy):
+        if signal_dst_rdy is None:
+            return (signal_src_rdy.value == 1)
+        else:
+            return (signal_src_rdy.value == 1) and (signal_dst_rdy.value == 1)
+
+    async def _monitor_recv(self):
+        """Watch the pins and reconstruct transactions."""
+
+        # Avoid spurious object creation by recycling
+        clkedge = RisingEdge(self.clock)
+        
+        while True:
+            await clkedge
+
+            if self.in_reset:
+                continue
+            
+            if self._is_valid_word(self.bus.src_rdy, self.bus.dst_rdy):
+                data_val = self.bus.data.value
+                data_val.big_endian = False
+                data_bytes = data_val.buff
+
+                vld = self.bus.vld.value
+
+                for offset in range(self._items):   
+                    if (vld[self._items-offset-1]):
+                        self.log.debug(f"ITEM {self.item_cnt}")
+                        self.log.debug(f"recieved item: {data_bytes[offset*self._item_width:(offset+1)*self._item_width]}")
+                        self.log.debug(f"word: {data_bytes}")
+                        self.log.debug(f"item vld: {vld[self._items-offset-1]}")
+                        self.log.debug(f"word vld: {vld}")
+                        self._recv(data_bytes[offset*self._item_width:(offset+1)*self._item_width])
+
+                        self.item_cnt += 1
---
+++ python/cocotbext/cocotbext/ofm/mvb/utils.py
@@ -0,0 +1,57 @@
+# utils.py: MVB utils
+# Copyright (C) 2024 CESNET z. s. p. o.
+# Author(s): Ondřej Schwarz <OndrejSchwarz@cesnet.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import random
+
+def get_mvb_params(items, params_dic):
+    cDelays = dict()
+    
+    if params_dic is None:
+        mode = 1
+        
+        #parameters for whole invalid words
+        wordDelayEnable_wt = 10 
+        wordDelayDisable_wt= 90
+        wordDelayLow = 0
+        wordDelayHigh = 50
+
+        #parameters for whole invalid items        
+        ivgEnable_wt = 3
+        ivgDisable_wt = 1
+        ivgLow = 0
+        ivgHigh = 2*items-1
+            
+    else:
+        #parameters for whole invalid words
+        mode = params_dic["mode"]
+        
+        wordDelayEnable_wt = params_dic["wordDelayEnable_wt"]
+        wordDelayDisable_wt= params_dic["wordDelayDisable_wt="]
+        wordDelayLow = params_dic["wordDelayLow"]
+        wordDelayHigh = params_dic["wordDelayHigh"]
+
+        #parameters for whole invalid items        
+        ivgEnable_wt = params_dic["ivgEnable_wt"]
+        ivgDisable_wt = params_dic["ivgDisable_wt"]
+        ivgLow = params_dic["ivgLow"]
+        ivgHigh = params_dic["ivgHigh"]
+
+
+    cDelays["wordDelayEn_wt"] = (wordDelayDisable_wt, wordDelayEnable_wt)
+    cDelays["wordDelay"] = range(wordDelayLow, wordDelayHigh)
+    cDelays["ivgEn_wt"] = (ivgDisable_wt, ivgEnable_wt)
+    cDelays["ivg"] = range(ivgLow, ivgHigh)
+
+    if mode == 0:
+        delays_fill = None
+    elif mode == 1:
+        delays_fill = 0
+    elif mode == 2:
+        delays_fill = random.randrange(0,256)
+    elif mode == 3:
+        delays_fill = 88 #88=X in ascii
+
+    return cDelays, mode, delays_fill
