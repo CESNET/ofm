c4caa9138f73700cca6ee070fb621c1781af95cb
Martin Spinler <spinler@cesnet.cz>
2024-06-06 10:07:14 +0200

    cocotb - generators: add EthernetRateLimiter

--- python/cocotbext/cocotbext/ofm/base/generators.py
+++ python/cocotbext/cocotbext/ofm/base/generators.py
@@ -1,4 +1,4 @@
-from .transaction import Transaction
+from .transaction import Transaction, IdleTransaction
 
 
 class IdleGenerator():
@@ -52,3 +52,55 @@ class IdleGenerator():
             'start': bool       # start of transaction was sent
             'end':   bool       # end of transaction was sent (implies transaction was completly sent)
         """
+
+
+class EthernetRateLimiter(IdleGenerator):
+    """
+    Limit throughput to achieve specified maximum rate on Ethernet by generating IdleTransaction.
+
+    Ensure the driver puts transaction with "end" argument.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self._bitrate = bitrate
+        self._current_rate = 0
+
+        # Add SFD, CRC, IPG; in items units
+        self._packet_overhead = 8 + 4 + 12
+
+    def configure(self, **kwargs):
+        super().configure(**kwargs)
+
+        #assert bits_per_item == 8
+        clk_freq = kwargs.get("clk_freq")
+        bits_per_word = kwargs.get("bits_per_word")
+        if clk_freq and bits_per_word:
+            self._cfg_complete = True
+
+            target_bitrate = self._bitrate / (clk_freq / 1_000_000)
+            # The self._target_rate value is percentual load of the bus.
+            # For example: 0.23 (23%) means 23 Data items for each 100 items,
+            # that is 23:77 data:idle items ratio
+            self._target_rate = target_bitrate / bits_per_word
+
+    def get(self, transaction, **kwargs):
+        assert self._cfg_complete
+        rate = (self._current_rate - self._target_rate) / self._target_rate
+        return int(max(0, rate))
+
+    def put(self, transaction, **kwargs):
+        assert self._cfg_complete
+
+        items = kwargs['items']
+        ir = self._current_rate
+
+        if not isinstance(transaction, IdleTransaction):
+            ir += items
+            if kwargs.get("end", False):
+                ir += self._packet_overhead
+
+        # decrease current rate with the expected target rate to maintain value near zero
+        ir -= items * self._target_rate
+        self._current_rate = 0 if ir < 0 else ir
