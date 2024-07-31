95c2cea24c8422bb3784083ea82572efaedbfacc
Radek Isa <isa@cesnet.cz>
2024-07-31 18:50:17 +0200

    CHECKSUM CALCURATOR UVM: [MAITENANCE] Simplify generating checksum in model

--- comp/mfb_tools/logic/checksum_calculator/uvm/tbench/env/scoreboard_cmp.sv
+++ comp/mfb_tools/logic/checksum_calculator/uvm/tbench/env/scoreboard_cmp.sv
@@ -7,43 +7,50 @@
 class chsum_calc_cmp #(MVB_DATA_WIDTH, MFB_META_WIDTH) extends uvm_common::comparer_base_ordered#(uvm_checksum_calculator::chsum_calc_item#(MVB_DATA_WIDTH, MFB_META_WIDTH), uvm_logic_vector::sequence_item#(MVB_DATA_WIDTH+1+MFB_META_WIDTH));
     `uvm_component_param_utils(uvm_checksum_calculator::chsum_calc_cmp #(MVB_DATA_WIDTH, MFB_META_WIDTH))
 
-    protected uvm_logic_vector::sequence_item #(MVB_DATA_WIDTH) tr_dut_data;
-    protected logic                                             bypass_dut;
-    protected logic[MFB_META_WIDTH-1 : 0]                       meta_dut;
-
     function new(string name, uvm_component parent = null);
         super.new(name, parent);
     endfunction
 
     virtual function int unsigned compare(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
-        int unsigned                                      ret = 1;
+        logic [MVB_DATA_WIDTH-1 : 0]  data_dut;
+        logic                         bypass_dut;
+        logic [MFB_META_WIDTH-1 : 0]  meta_dut;
+        int unsigned  ret = 1;
 
-        `uvm_info(get_type_name(), message(tr_model, tr_dut), UVM_HIGH)
 
-        tr_dut_data      = uvm_logic_vector::sequence_item #(MVB_DATA_WIDTH)::type_id::create("tr_dut_data");
-        tr_dut_data.data = tr_dut.data[MVB_DATA_WIDTH-1 : 0];
-        bypass_dut       = tr_dut.data[MVB_DATA_WIDTH];
-        meta_dut         = tr_dut.data[MVB_DATA_WIDTH+1+MFB_META_WIDTH-1 : MVB_DATA_WIDTH];
+        {meta_dut, bypass_dut, data_dut} = tr_dut.data;
 
         if (tr_model.bypass) begin
             ret = 1;
         end else begin
             //|= is different that  &=. Be carefull when use what.
-            ret &= tr_dut_data.data === tr_model.data_tr.data;
-            ret &= bypass_dut       === tr_model.bypass;
-            ret &= meta_dut         === tr_model.meta;
+            ret &= data_dut    === tr_model.data;
+            ret &= (bypass_dut === 0);
+            ret &= meta_dut    === tr_model.meta;
         end
 
         return ret;
     endfunction
 
-    virtual function string message(MODEL_ITEM tr_model, DUT_ITEM tr_dut);
-        string msg = "\n";
-        msg = {msg, $sformatf("\n\tDUT PACKET %s\n\n",  tr_dut_data.convert2string())};
-        msg = {msg, $sformatf("\n\tDUT BYPASS %h\n\n",  bypass_dut)};
-        msg = {msg, $sformatf("\n\tDUT META   %h\n\n",  meta_dut)};
-        msg = {msg, $sformatf("\n\tMODEL PACKET%s\n\n",  tr_model.convert2string())};
+    
+    virtual function string dut_item2string(DUT_ITEM tr);
+        logic [MVB_DATA_WIDTH-1 : 0]  data;
+        logic                         bypass;
+        logic [MFB_META_WIDTH-1 : 0]  meta;
+        string msg;
+
+        
+        {meta, bypass, data} = tr.data;
+        msg = tr.time2string();
+        msg = {msg, $sformatf("\n\tbypass %b",  bypass)};
+        msg = {msg, $sformatf("\n\tmeta   %h",  meta)};
+        msg = {msg, $sformatf("\n\tdata   %h",  data)};
         return msg;
     endfunction
+
+    virtual function string model_item2string(MODEL_ITEM tr);
+        return tr.convert2string();
+    endfunction
+
 endclass
 
