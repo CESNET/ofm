6322a2e6c97bbc2c5476211bcff2d0a57e97c5f3
xschwao00 <xschwao00@stud.fit.vutbr.cz>
2024-08-07 12:25:25 +0200

    MVB_HASH_TABLE_SIMPLE: [FEATURE] add MVB_HASH_TABLE_SIMPLE component and cocotb verification

---
+++ comp/mvb_tools/storage/mvb_hash_table_simple/cocotb/drivers.py
@@ -0,0 +1,5 @@
+from cocotbext.ofm.mvb.drivers import MVBDriver
+
+class MVB_HASH_TABLE_SIMPLE_Driver(MVBDriver):
+    _signals = {"data": "key", "vld": "vld", "src_rdy": "src_rdy", "dst_rdy": "dst_rdy"}
+
---
+++ comp/mvb_tools/storage/mvb_hash_table_simple/cocotb/monitors.py
@@ -0,0 +1,18 @@
+from cocotbext.ofm.mvb.monitors import MVBMonitor
+
+class MVB_HASH_TABLE_SIMPLE_Monitor(MVBMonitor):
+    _signals = ["data", "match", "vld", "src_rdy", "dst_rdy"]
+ 
+    def recieve_data(self, data, offset):
+        match_val = self.bus.match.value
+        match_val.big_endian = False
+
+        self.log.debug(f"MATCH: {match_val}")
+
+        if match_val[offset] == 1:
+            self._recv((data[offset*self._item_width : (offset+1)*self._item_width], 1)) #k match signalu pridat offset
+        else:
+            self._recv((self._item_width*b'\x00', 0))
+             
+
+
