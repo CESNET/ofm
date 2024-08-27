62500258762a7edde492f01ad02b7339dd81ff3d
Jakub Cabal <cabal@cesnet.cz>
2024-08-27 08:37:43 +0200

    MVB_CHANNEL_ROUTER: [MAINTENANCE] Update description of default mode and opt mode in DeviceTree

--- comp/mvb_tools/flow/channel_router/DevTree.tcl
+++ comp/mvb_tools/flow/channel_router/DevTree.tcl
@@ -1,8 +1,9 @@
 # 1. name         - instantion name inside device tree hierarchy
 # 2. base         - base address on MI bus
 # 3. src_chan     - number of source channels ($reg_size = $src_chan*0x4)
-# 4. default_mode - number of default mode (optional)
-# 5. opt_mode     - number of opt_mode (optional)
+# 4. default_mode - default mode number (possible values: -1 = undefined; 0 = stay on src channel; 1 = round-robin; 2 = round-robin in each group / src channel)
+# 5. opt_mode     - opt mode number (possible values: -1 = undefined; 0 = false; 1 = true)
+# The default_mode and opt_mode are more explained in the MVB_CHANNEL_ROUTER documentation.
 proc dts_mvb_channel_router {name base src_chan {default_mode -1} {opt_mode -1}} {
     set size [expr $src_chan*0x4]
     set    ret ""
